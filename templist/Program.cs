182af0487c7a038412ad94fc26930a0b591d1431
Ryan Novak <rnovakdevacc@gmail.com>
2023-07-13 20:30:57 -0400

    Implement task creation
    Added ability for user to add tasks to list
    
    Prints contents of the list on exit

--- templist/Program.cs
+++ templist/Program.cs
@@ -1,3 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, Lists!");
+﻿string [] todo = new string[10];
+int counter = 0;
+bool exit = false;
 
+while (exit == false) {
+    Console.Write("Add (a), Complete Task (c), Remove (r) Quit (q): ");
+    // TODO: Make sure this only accepts options given
+    char operation = Convert.ToChar(Console.ReadLine());
+    switch (operation)
+    {
+        case 'a':
+            Console.Write("Task name: ");
+            todo[counter] = Console.ReadLine();
+            counter++;
+            break;
+        case 'c':
+            // TODO: implement this case
+            break;
+        case 'q':
+            exit = true;
+            break;
+        default:
+            Console.WriteLine("Please input one of the characters provided.");
+            break;
+    }
+}
+for (int i = 0; i < todo.Length; i++)
+{
+    if (todo[i] != null)
+    {
+        Console.WriteLine("[ ]" + todo[i]);
+    }
+}
+Console.ReadLine();

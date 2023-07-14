c9c0e8f5c7db1a95485574289bcaac6357e9421a
Ryan Novak <rnovakdevacc@gmail.com>
2023-07-13 21:00:55 -0400

    Implement task completion
    Added the ability to mark tasks added to the list as complete

--- templist/Program.cs
+++ templist/Program.cs
@@ -1,4 +1,6 @@
-﻿string [] todo = new string[10];
+﻿// TODO: create a class for tasks that stores whether or not they are completed
+// TODO: refactor/comment
+string [] todo = new string[10];
 int counter = 0;
 bool exit = false;
 
@@ -10,11 +12,22 @@ while (exit == false) {
     {
         case 'a':
             Console.Write("Task name: ");
-            todo[counter] = Console.ReadLine();
+            todo[counter] = "[ ] " + Console.ReadLine();
             counter++;
             break;
         case 'c':
-            // TODO: implement this case
+            for (int i = 1; i <= todo.Length; i++)
+            {
+                if (todo[i - 1] != null)
+                {
+                    Console.WriteLine(i + todo[i - 1]);
+                }
+            }
+            Console.Write("Enter task number: ");
+            int  taskNum = Convert.ToInt32(Console.ReadLine()) - 1;
+            string completedTask = todo[taskNum];
+            completedTask = "[x]" + completedTask.Substring(3, (completedTask.Length - 3));
+            todo[taskNum] = completedTask;
             break;
         case 'q':
             exit = true;
@@ -28,7 +41,7 @@ for (int i = 0; i < todo.Length; i++)
 {
     if (todo[i] != null)
     {
-        Console.WriteLine("[ ]" + todo[i]);
+        Console.WriteLine(todo[i]);
     }
 }
 Console.ReadLine();

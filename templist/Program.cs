db5dc49e84ecf36c93d5c7ec67e55abbdc0241cd
Ryan Novak <rnovakdevacc@gmail.com>
2023-07-16 10:10:18 -0400

    Implement Task class
    Created Task class to store task name and completion status
    
    Implemented new class wherever task names were accessed

--- templist/Program.cs
+++ templist/Program.cs
@@ -1,114 +1,138 @@
-﻿// TODO: create a class for tasks that stores whether or not they are completed
-using static System.Collections.Specialized.BitVector32;
+﻿using static System.Collections.Specialized.BitVector32;
 using System;
 
-static void ShowList(string[] items)
+
+namespace Templist
 {
-    for (int i = 0; i < items.Length; i++)
+    class Program
     {
-        if (items[i] != null)
+        static void Main(string[] args)
         {
-            Console.WriteLine(items[i]);
-        }
-    }
-    return;
-}
+            string operations = "1: Add\n2: Complete Task\n3: Remove Task\n4: Show Tasks\n5: Quit\n: ";
+            // initialize task list array
+            Tasks[] todo = new Tasks[10];
+            // initialize task counter
+            int counter = 0;
+            // initialize exit condition for main loop
+            bool exit = false;
 
-
-string operations = "1: Add\n2: Complete Task\n3: Remove Task\n4: Show Tasks\n5: Quit\n: ";
-// initialize task list array
-string [] todo = new string[10];
-// initialize task counter
-int counter = 0;
-// initialize exit condition for main loop
-bool exit = false;
-
-while (exit == false) {
-    // print options to user
-    Console.Write(operations);
-    int selectInt = 0;
-    // prompt user until valid input is provided
-    while (true)
-    {
-        string? selectStr = Console.ReadLine();
-        // if input is valid, output integer to selectInt and exit
-        if (int.TryParse(selectStr, out selectInt) && int.Parse(selectStr) > 0 && int.Parse(selectStr) <= 5)
-        {
-            break;
-        }
-        else
-        {
-            Console.WriteLine("Please input an integer between 1 and 5");
-            Console.Write(operations);
-        }
-    }
-
-    // choose operation to perform based on user's input
-    switch (selectInt)
-    {
-        // add task
-        case 1:
-            Console.Write("Task name: ");
-            todo[counter] = "[ ] " + Console.ReadLine();
-            counter++;
-            break;
-
-        // mark a task complete (x in [])
-        case 2:
-            for (int i = 1; i <= todo.Length; i++)
+            while (exit == false)
             {
-                // check that the selected task exists
-                if (todo[i - 1] != null)
+                // print options to user
+                Console.Write(operations);
+                int selectInt = 0;
+                // prompt user until valid input is provided
+                while (true)
                 {
-                    Console.WriteLine(i + todo[i - 1]);
+                    string? selectStr = Console.ReadLine();
+                    // if input is valid, output integer to selectInt and exit
+                    if (int.TryParse(selectStr, out selectInt) && int.Parse(selectStr) > 0 && int.Parse(selectStr) <= 5)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Please input an integer between 1 and 5");
+                        Console.Write(operations);
+                    }
                 }
-                // TODO: tell the user if the task doesn't exist
+
+                // choose operation to perform based on user's input
+                switch (selectInt)
+                {
+                    // add task
+                    case 1:
+                        Console.Write("Task name: ");
+                        todo[counter] = new Tasks("[ ] " + Console.ReadLine());
+                        counter++;
+                        break;
+
+                    // mark a task complete (x in [])
+                    case 2:
+                        // print numbered task list
+                        for (int i = 1; i <= todo.Length; i++)
+                        {
+                            if (todo[i - 1] != null)
+                            {
+                                Console.WriteLine(i + todo[i - 1].TaskName);
+                            }
+                        }
+                        // initialize integer to store input string after int conversion
+                        int taskNum = 0;
+                        // prompt user until valid input is provided
+                        while (true)
+                        {
+                            Console.Write("Enter task number: ");
+                            string? taskStr = Console.ReadLine();
+
+                            // if input is valid, output integer to taskNum and exit
+                            if (int.TryParse(taskStr, out taskNum) && int.Parse(taskStr) > 0 && int.Parse(taskStr) <= todo.Length)
+                            {
+                                break;
+                            }
+                            else
+                            {
+                                Console.WriteLine($"Please input an integer between 1 and {todo.Length}");
+                            }
+                        }
+
+                        // TODO: implement this using the completed attribute of the Task class
+                        // check off the selected task
+                        string completedTask = todo[taskNum - 1].TaskName;
+                        completedTask = "[x]" + completedTask.Substring(3, (completedTask.Length - 3));
+                        todo[taskNum - 1].TaskName = completedTask;
+                        break;
+
+                    // print current list contents
+                    case 4:
+                        ShowList(todo);
+                        Console.Write("Press enter to continue:");
+                        Console.ReadLine();
+                        break;
+
+                    // exit the program and print your final list
+                    case 5:
+                        exit = true;
+                        break;
+                    default:
+                        Console.WriteLine("Please input one of the characters provided.");
+                        break;
+
+                }
             }
-            // initialize integer to store input string after int conversion
-            int taskNum = 0;
-            // prompt user until valid input is provided
-            while (true)
-            {
-                Console.Write("Enter task number: ");
-                string? taskStr = Console.ReadLine();
 
-                // if input is valid, output integer to taskNum and exit
-                if (int.TryParse(taskStr, out taskNum) && int.Parse(taskStr) > 0 && int.Parse(taskStr) <= todo.Length)
-                {
-                    break;
-                }
-                else
-                {
-                    Console.WriteLine($"Please input an integer between 1 and {todo.Length}");
-                }
-            }
-
-            // check off the selected task
-            string completedTask = todo[taskNum - 1];
-            completedTask = "[x]" + completedTask.Substring(3, (completedTask.Length - 3));
-            todo[taskNum - 1] = completedTask;
-            break;
-
-        // print current list contents
-        case 4:
+            // print the final list
             ShowList(todo);
-            Console.Write("Press enter to continue:");
+            // leave list displayed until key press
             Console.ReadLine();
-            break;
 
-        // exit the program and print your final list
-        case 5:
-            exit = true;
-            break;
-        default:
-            Console.WriteLine("Please input one of the characters provided.");
-            break;
 
+
+
+            static void ShowList(Tasks[] items)
+            {
+                for (int i = 0; i < items.Length; i++)
+                {
+                    if (items[i] != null)
+                    {
+                        Console.WriteLine(items[i].TaskName);
+                    }
+                }
+                return;
+            }
+
+        }
+    }
+
+
+    public class Tasks
+    {
+        public string TaskName { get; set; }
+        bool complete = false;
+
+        public Tasks(string taskName)
+        {
+            TaskName = taskName;
+        }
     }
 }
-
-// print the final list
-ShowList(todo);
-// leave list displayed until key press
-Console.ReadLine();
-
